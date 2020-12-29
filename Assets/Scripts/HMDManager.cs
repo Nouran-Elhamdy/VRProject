20a5b0c8df04c20053b9f2f03cd6f5ebaeaa313b
Nouran-Elhamdy <nouranelhamdy@gmail.com>
2020-12-29 12:48:02 +0200

    Uploading VR Project

---
+++ Assets/Scripts/HMDManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR;
+using UnityEngine;
+
+public class HMDManager : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("is Device active: " + XRSettings.isDeviceActive);
+        Debug.Log("Active Device name: " + XRSettings.loadedDeviceName);
+    }
+
+   
+}
